278c016ddcafe03f9217895432529a71cd8c0263
Emil <emilzaripov48@gmail.com>
2024-05-06 22:12:07 +0300

    add projects list

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -2,6 +2,13 @@
 useHead({
   title: "EasyVideo - Account",
 });
+
+const colors = ref([
+  "bg-red-500",
+  "bg-green-500",
+  "bg-indigo-500",
+  "bg-orange-500",
+]);
 </script>
 
 <template>
@@ -18,13 +25,40 @@ useHead({
         </div>
         <AppButton>
           <div class="flex gap-3 items-center">
-            <p>Создать проект</p>
+            <p class="flex-1">Создать проект</p>
+            <figure>
+              <img
+                style="
+                  filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(49deg)
+                    brightness(103%) contrast(103%);
+                "
+                class="w-4 h-4"
+                src="~/assets/img/icons/actions/plus.svg"
+                alt=""
+              />
+            </figure>
           </div>
         </AppButton>
       </div>
     </header>
-    <section class="w-full h-screen p-4">
-      <div class="grid grid-cols-5"></div>
+    <section class="w-full h-screen pt-[85px]">
+      <div class="grid grid-cols-6">
+        <div
+          v-for="i in 7"
+          class="p-5 cursor-pointer hover:bg-zinc-900 rounded-lg"
+        >
+          <div class="flex flex-col gap-3">
+            <div
+              :class="[colors[Math.floor(Math.random() * 4)]]"
+              class="h-[160px] w-full rounded-lg"
+            ></div>
+            <div>
+              <h3 class="text-white text-medum text-base">Без названия</h3>
+              <p class="text-zinc-500 text-sm">Редактирован 1 день назад</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
