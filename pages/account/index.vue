1be7c90d8f7bc63aafa47e188872e52ae3746b25
Pororo274 <emilzaripov48@gmail.com>
2024-05-07 16:04:26 +0300

    add close modal button

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -13,6 +13,10 @@ const colors = ref([
   "bg-indigo-500",
   "bg-orange-500",
 ]);
+
+const isModalActive = ref(false);
+
+const { user } = useUser();
 </script>
 
 <template>
@@ -23,11 +27,13 @@ const colors = ref([
           <div
             class="w-6 h-6 rounded-full bg-indigo-600 flex items-center justify-center"
           >
-            <p class="text-white font-medium text-xs">U</p>
+            <p class="text-white font-medium text-xs">
+              {{ user.username[0].toUpperCase() }}
+            </p>
           </div>
-          <h2 class="text-white text-base">Username</h2>
+          <h2 class="text-white text-base">{{ user.username }}</h2>
         </div>
-        <AppButton>
+        <AppButton @click="isModalActive = true">
           <div class="flex gap-3 items-center">
             <p class="flex-1">Создать проект</p>
             <figure>
@@ -46,7 +52,10 @@ const colors = ref([
       </div>
     </header>
     <section class="w-full h-screen pt-[85px]">
-      <div class="w-full h-screen fixed z-50 top-0 left-0 backdrop-blur-sm">
+      <div
+        class="w-full h-screen fixed z-50 top-0 left-0 backdrop-blur-sm"
+        v-if="isModalActive"
+      >
         <div
           class="w-full h-full absolute top-0 left-0 bg-black opacity-70"
         ></div>
@@ -54,7 +63,20 @@ const colors = ref([
           class="w-[30%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-zinc-900 rounded-lg"
         >
           <div class="p-5 border-b border-b-zinc-800">
-            <h2 class="text-white text-xl font-medium">Новый проект</h2>
+            <div class="flex items-center justify-between">
+              <h2 class="text-white text-xl font-medium">Новый проект</h2>
+              <figure @click="isModalActive = false" class="cursor-pointer">
+                <img
+                  style="
+                    filter: invert(100%) sepia(0%) saturate(0%)
+                      hue-rotate(49deg) brightness(103%) contrast(103%);
+                  "
+                  class="w-4 h-4"
+                  src="~/assets/img/icons/actions/close.svg"
+                  alt=""
+                />
+              </figure>
+            </div>
           </div>
           <div class="p-5">
             <AppForm action="/projects">
